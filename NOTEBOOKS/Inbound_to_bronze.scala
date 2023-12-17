529d41bc305fb7540739ef258ec4442359331087
hugocerqueira268 <hugocerqueira268>
2023-12-17 23:06:21 +0000

    notebooks_pipeline

---
+++ NOTEBOOKS/Inbound_to_bronze.scala
@@ -0,0 +1,47 @@
+// Databricks notebook source
+// MAGIC %python
+// MAGIC dbutils.fs.ls("/mnt/dados/in_bond/")
+
+// COMMAND ----------
+
+val path = "dbfs:/mnt/dados/in_bond/dados_brutos_imoveis.json"
+val dados = spark.read.json(path)
+
+// COMMAND ----------
+
+display(dados)
+
+// COMMAND ----------
+
+val dados_anuncio= dados.drop("imagens", "usuario")
+display(dados_anuncio)
+
+
+// COMMAND ----------
+
+import org.apache.spark.sql.functions.col
+
+// COMMAND ----------
+
+val df_bronze = dados_anuncio.withColumn("id", col("anuncio.id"))
+display(df_bronze)
+
+// COMMAND ----------
+
+val path = "dbfs:/mnt/dados/bronze/dataset_imoveis"
+df_bronze.write.format("delta").mode(SaveMode.Overwrite).save(path)
+
+
+// COMMAND ----------
+
+display(df_bronze)
+
+// COMMAND ----------
+
+val path = "dbfs:/mnt/dados/bronze/dataset_imoveis"
+df_bronze.write.format("delta").mode(SaveMode.Overwrite).save(path)
+
+
+// COMMAND ----------
+
+
