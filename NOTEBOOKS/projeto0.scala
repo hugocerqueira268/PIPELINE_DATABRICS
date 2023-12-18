c8bfe726a37ef09252b5f245c641203c87d37dee
hugocerqueira268 <hugocerqueira268>
2023-12-18 22:53:49 +0000

    Comit2

---
+++ NOTEBOOKS/projeto0.scala
@@ -0,0 +1,11 @@
+// Databricks notebook source
+dbutils.fs.mkdirs("/FileStore/tables/arquivos_curso")
+
+
+// COMMAND ----------
+
+display(dbutils.fs.ls("/FileStore/tables/arquivos_curso"))
+
+// COMMAND ----------
+
+
